c642dd752fa6bc60b774e186d169bcdc2f4534c9
Jpastran <jpastran93@gmail.com>
2015-12-02 15:02:53 -0500

    Correcion de BD Correcion de lineas

--- Conexion.php
+++ Conexion.php
@@ -1,4 +1,4 @@
-﻿<?php
-	mysql_connect("mysql.hostinger.co","u303471679_est","123456");
-	mysql_select_db("u303471679_est");	
+<?php
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("feria_U");
 ?>
