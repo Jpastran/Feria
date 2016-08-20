a6a395ca60441cceb464169cf5fa03aaeb955731
Jpastran <jpastran93@gmail.com>
2016-08-19 22:10:10 -0500

    Agregado paginacion a programas

--- js/scripts-gestor.js
+++ js/scripts-gestor.js
@@ -516,7 +516,8 @@ function eliminarProg() {
 
 function cargarProg() {
     var parametros = {
-        "Lista": "Lista"
+        "Lista": "Lista",
+        "Pagina": paginar("pag")
     };
     $.ajax({
         data: parametros,
@@ -531,6 +532,35 @@ function cargarProg() {
             $("#respu").html("Error Al Conectarse Al Servidor");
         }
     });
+}
+
+function paginar(param) {
+    /* Obtener la url completa */
+    url = document.URL;
+    /* Buscar a partir del signo de interrogación ? */
+    url = String(url.match(/\?+.+/));
+    /* limpiar la cadena quitándole el signo ? */
+    url = url.replace("?", "");
+    /* Crear un array con parametro=valor */
+    url = url.split("&");
+
+    /* 
+     Recorrer el array url
+     obtener el valor y dividirlo en dos partes a través del signo = 
+     0 = parametro
+     1 = valor
+     Si el parámetro existe devolver su valor
+     */
+    x = 0;
+    while (x < url.length)
+    {
+        p = url[x].split("=");
+        if (p[0] == param)
+        {
+            return decodeURIComponent(p[1]);
+        }
+        x++;
+    }
 }
 
 function ocultarProg() {
